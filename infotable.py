d104bf18e9f5868edcc6a70aa7c4df780917ce37
max180692 <111618832+max180692@users.noreply.github.com>
2022-09-04 17:12:33 +0300

    Update infotable.py

--- infotable.py
+++ infotable.py
@@ -1,57 +1,52 @@
 import lxml
 from bs4 import BeautifulSoup as bs
 
+# Класс по парсингу таблиц из сайта
+
 class Table:
     def __init__(self,source):
-        self.table = False
-        self.source = source
-        self.soup = None
-        self.body = None
+        self.soup = bs(source,'lxml')
+        self.soup_body = None
+        self.list_table = None
         self.inform = False
         self.not_table = None
 
-    def find_table(self):
-        self.soup = bs(self.source,'lxml')
-       
-        if self.soup.main:
-            self.soup_body = self.soup.main
-            self.list_table = self.soup_body.find_all('tbody')
-            self.not_table = self.soup_body.find('div',{'data-test':'instrument-header-details'})
-            if self.not_table:
-                self.inform = True
-            else:
-                self.inform = False
-        elif self.soup.section:
-            self.soup_body = self.soup.section
-            self.list_table = self.soup_body.find_all('tbody')
-            self.not_table = self.soup_body.find('div',{'data-test':'instrument-header-details'})
-            if self.not_table:
-                self.inform = True
-            else:
-                self.inform = False
-            
+#Проверка наличии таблицы
 
-
-    def find_not_table(self):
-        self.find_table()
-        list_title = self.soup_body.find('h1')
-        information = self.soup_body.find('div',{'data-test':'instrument-header-details'})
-        if information:
-            print(list_title.text)
-            print(information.text)
+    def check_body_table(self):
+        self.list_table = self.soup_body.find_all('tbody')
+        self.not_table = self.soup_body.find('div',{'class':'fullHeaderTwoColumnPage--top'})
+        if self.not_table:
             self.inform = True
         else:
             self.inform = False
+            
+        
+# Поиск елементов в разных блоках
 
+    def find_table(self):
+        if self.soup.main:
+            self.soup_body = self.soup.main
+            self.check_body_table()
+        elif self.soup.section:
+            self.soup_body = self.soup.section
+            self.check_body_table()
+            
+#Функция поиска если не таблица
+
+    def find_not_table(self):
+        list_title = self.soup_body.find('h1')
+        information = self.soup_body.find('div',{'class':'fullHeaderTwoColumnPage--top'}).select_one('div.top.bold.inlineblock').select('span')
+        print(list_title.text)
+        print(information[0].text,information[1].text,information[2].text,information[3].text,information[4].text)
         print('+++++',self.inform)
 
+#Вывод информации с сайта
 
     def info_tables(self):
         self.find_table()
         if self.inform:
-            list_title = self.soup_body.find('h1')
-            print(list_title.text)
-            print(self.not_table.text)
+            self.find_not_table()
         else:
             if len(self.list_table)>1:
                 list_title = self.soup_body.find_all('h2')
@@ -60,7 +55,7 @@ class Table:
                         title = list_title[i].text
                         print(title)
                         list_tag_td = self.list_table[i].find_all('td')
-                        all_text = "|".join([tag_td.text.strip() for tag_td in list_tag_td]).replace('|||','\n')
+                        all_text = "|".join([tag_td.text.strip() for tag_td in list_tag_td]).replace('|||','\n').replace('||','\n')
                         print(all_text,'\n')
             elif len(self.list_table) == 1:
                 list_title = self.soup_body.find_all('h1')
@@ -69,7 +64,7 @@ class Table:
                         title = list_title[i].text
                         print(title)
                         list_tag_td = self.list_table[i].find_all('td')
-                        all_text = "|".join([tag_td.text.strip() for tag_td in list_tag_td]).replace('|||','\n')
+                        all_text = "|".join([tag_td.text.strip() for tag_td in list_tag_td]).replace('|||','\n').replace('||','\n')
                         print(all_text,'\n')
 
 
