605eb5438d650b786d11fb0b56697ddb8d7c145d
max180692 <111618832+max180692@users.noreply.github.com>
2022-09-19 10:45:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,31 @@
+from re import U
 from category import Category
 from connectrequests import NewConnect
 from  connectselen import GetHtmlPage
 from investing import Investing
+from settingsparser import URL
 
 
 def main():
-    url = 'https://ru.investing.com'
+    url = URL
     category = Category()
     newconect = NewConnect()
     invest = Investing()
-    if category.get_check():
-        invest.set_html_page()
-        invest.get_info()
-    else:
-        html_page = newconect.connect(url)
-        if html_page:
-            invest.set_html_page(html_page=html_page)
+    while True:
+        if category.get_check():
+            invest.set_html_page()
             invest.get_info()
         else:
-            ghp = GetHtmlPage()
-            html_page = ghp.get_sourcepage(url)
-            invest.set_html_page(html_page=html_page)
-            invest.get_info()
-            
+            html_page = newconect.connect(url)
+            if html_page:
+                invest.set_html_page(html_page=html_page)
+                invest.get_info()
+            else:
+                ghp = GetHtmlPage()
+                html_page = ghp.get_sourcepage(url)
+                invest.set_html_page(html_page=html_page)
+                invest.get_info()
+                
             
 
 
