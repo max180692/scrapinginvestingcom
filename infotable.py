7536b922b3c639c19c5204569b374e35aa97df8d
max180692 <111618832+max180692@users.noreply.github.com>
2022-09-19 10:45:20 +0300

    Update infotable.py

--- infotable.py
+++ infotable.py
@@ -1,70 +1,73 @@
 import lxml
 from bs4 import BeautifulSoup as bs
+from myjson import MyJson
 
 # Класс по парсингу таблиц из сайта
 
 class Table:
     def __init__(self,source):
-        self.soup = bs(source,'lxml')
-        self.soup_body = None
-        self.list_table = None
-        self.inform = False
-        self.not_table = None
+        self.__soup = bs(source,'lxml')
+        self.__soup_body = None
+        self.__list_table = None
+        self.__inform = False
+        self.__not_table = None
+        self.__table = list()
 
 #Проверка наличии таблицы
 
-    def check_body_table(self):
-        self.list_table = self.soup_body.find_all('tbody')
-        self.not_table = self.soup_body.find('div',{'class':'fullHeaderTwoColumnPage--top'})
-        if self.not_table:
-            self.inform = True
+    def __check_body_table(self):
+        self.__list_table = self.__soup_body.select('table.genTbl')
+        self.__not_table = self.__soup_body.find('div',{'class':'fullHeaderTwoColumnPage--top'})
+        if self.__not_table:
+            self.__inform = True
         else:
-            self.inform = False
+            self.__inform = False
             
         
 # Поиск елементов в разных блоках
 
-    def find_table(self):
-        if self.soup.main:
-            self.soup_body = self.soup.main
-            self.check_body_table()
-        elif self.soup.section:
-            self.soup_body = self.soup.section
-            self.check_body_table()
+    def __find_table(self):
+        if self.__soup.main:
+            self.__soup_body = self.__soup.main
+            self.__check_body_table()
+        elif self.__soup.section:
+            self.__soup_body = self.__soup.section
+            self.__check_body_table()
             
 #Функция поиска если не таблица
 
-    def find_not_table(self):
-        list_title = self.soup_body.find('h1')
-        information = self.soup_body.find('div',{'class':'fullHeaderTwoColumnPage--top'}).select_one('div.top.bold.inlineblock').select('span')
-        print(list_title.text)
-        print(information[0].text,information[1].text,information[2].text,information[3].text,information[4].text)
-        print('+++++',self.inform)
+    def __find_not_table(self):
+        list_title = self.__soup_body.find('h1')
+        information = self.__not_table.select_one('div.top.bold.inlineblock').select('span')
+        myjson = MyJson(list_title.text,information)
+        print(myjson.generate_json())
+
 
 #Функция вывода если таблица
 
-    def output_info_table(self,list_title):
-        if len(list_title) == len(self.list_table):
+    def __output_info_table(self,list_title):
+        if len(list_title) == len(self.__list_table):
             for i in range(len(list_title)):
                 title = list_title[i].text
-                print(title)
-                list_tag_td = self.list_table[i].find_all('td')
-                all_text = "|".join([tag_td.text.strip() for tag_td in list_tag_td]).replace('|||','\n').replace('||','\n')
-                print(all_text,'\n')
+                list_tag_td = self.__list_table[i].find_all('tr')
+                myjson = MyJson(title, list_tag_td)
+                self.__table.append(myjson.generate_json())
 
 #Вывод информации с сайта
 
     def info_tables(self):
-        self.find_table()
-        if self.inform:
-            self.find_not_table()
+        self.__find_table()
+        if self.__inform:
+            self.__find_not_table()
         else:
-            if len(self.list_table)>1:
-                list_title = self.soup_body.find_all('h2')
-                self.output_info_table(list_title)
-            elif len(self.list_table) == 1:
-                list_title = self.soup_body.find_all('h1')
-                self.output_info_table(list_title)
+            if len(self.__list_table)>1:
+                list_title = self.__soup_body.find_all('h2')
+                self.__output_info_table(list_title)
+                print(self.__table)
+            elif len(self.__list_table) == 1:
+                list_title = self.__soup_body.find_all('h1')
+                self.__output_info_table(list_title)
+                print(self.__table)
 
 
 
